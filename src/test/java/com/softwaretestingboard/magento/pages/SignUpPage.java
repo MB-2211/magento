eb1716526b8d7e04f2b5c7815634bc7f37d46e24
Moiz Bandookwala <moizbandookwala@gmail.com>
2024-12-23 13:28:52 +0530

    Add SignUpPage

---
+++ src/test/java/com/softwaretestingboard/magento/pages/SignUpPage.java
@@ -0,0 +1,43 @@
+package com.softwaretestingboard.magento.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+// page_url = https://magento.softwaretestingboard.com/customer/account/create/
+public final class SignUpPage {
+    @FindBy(id = "firstname")
+    private WebElement firstname;
+
+    @FindBy(id = "lastname")
+    private WebElement lastname;
+
+    @FindBy(id = "email_address")
+    private WebElement emailId;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "password-confirmation")
+    private WebElement passwordConfirmation;
+
+    @FindBy(css = "button[class='action submit primary']")
+    private WebElement signUpButton;
+
+    public SignUpPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void fill(String firstname, String lastname, String emailId, String password) {
+        this.firstname.sendKeys(firstname);
+        this.lastname.sendKeys(lastname);
+        this.emailId.sendKeys(emailId);
+        this.password.sendKeys(password);
+        this.passwordConfirmation.sendKeys(password);
+    }
+
+    public void clickSignUpButton() {
+        signUpButton.click();
+    }
+}
